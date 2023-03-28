e5841282fd6c0bb1e1da2ef489f19a5256606119
JacobSmart1 <smati.houssam@gmail.com>
2023-03-28 14:49:49 +0300

    animation text

---
+++ assets/js/animation.js
@@ -0,0 +1,7 @@
+/*==================== ANIMATION TEXT ====================*/
+var typed = new Typed('.typer', {
+    strings: ['Software Engineer', 'Backend Developer', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  });
